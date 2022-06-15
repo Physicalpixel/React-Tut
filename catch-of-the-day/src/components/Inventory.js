b7b1e7c122f66b7c98f03055086611fe768f544d
Physicalpixel <preethi.karan91@gmail.com>
2022-06-15 14:11:15 -0400

    App, Header, Order, Inventory, Video7

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+class Inventory extends React.Component {
+  render() {
+    return <div className="Inventory">Inventory !!!</div>;
+  }
+}
+
+export default Inventory;
